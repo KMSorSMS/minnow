955fd762daf74737b5d9d4328c3fec3a7484eb5c
liam <yzwliam@126.com>
2024-02-18 11:05:03 +0000

    improve function find_replace_addStr

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -13,7 +13,8 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     has_last = true;
   }
   first_unacceptable_index = first_unassembled_index + output_.writer().available_capacity();
-  if(first_index < first_unassembled_index && first_index + data.size() <= first_unassembled_index){//说明是之前assemble过的
+  if ( first_index < first_unassembled_index
+       && first_index + data.size() <= first_unassembled_index ) { // 说明是之前assemble过的
     return;
   }
   if ( !data.empty() && first_index < first_unacceptable_index ) {
@@ -78,72 +79,36 @@ uint64_t Reassembler::find_replace_addStr( string& input_str, const uint64_t& fi
   }
   set<pair<uint64_t, string>>::iterator iter_down = data_set.upper_bound( { first_index, "" } );
   set<pair<uint64_t, string>>::iterator iter_up = data_set.upper_bound( { first_index + input_str.size(), "" } );
-  // 不管是否存在有index比first_index大，都会让iter_down往前走一个，这个index一定是比first_index小或等于的，除非所有的都比first_index大
-  // 边界严谨一点
-  if ( iter_down != data_set.begin() ) { // 因为如果上界是开始，说明全部都比插入的index大，不移动iter，反之才移动
+  //思路是对于上界的划分精细
+  if(iter_down!=data_set.begin()){
     iter_down--;
+  }else if(iter_down==iter_up){//这个说明data_set的所有index都比first_index大，那么就需要在iter_down == iter_up的时候去调整iter_down 向后
+    iter_up++;
   }
-
-  uint64_t count_insert = 0; // 记录插入的bytes数
-  uint64_t start_index = first_index;
-  string leftStr = {};
-  string rightStr = input_str;
-  if ( iter_down->second.size() + iter_down->first <= start_index ) { // 说明底界和插入部分没有交集，就往上走
-    iter_down++;
-  }
+  //前面保证了iter_down 不会取到end
+  uint64_t count_insert = 0,start_index = first_index; // 记录插入的bytes数
+  string opeStr = input_str;
+  //截取操作进行规范，我们需要定义关键位置：理解这几个公式需要画图
+  uint64_t leftDownIndex=0 ,leftUpIndex=0, rightDownIndex=0,rightUpindex=0 ;
   // 开始遍历分割字符串
   for ( ; iter_down != iter_up; iter_down++ ) {
-    // 先对边界情况进行检验，看看是否有在iter_down的左右边是否有字符串
-    if ( start_index < iter_down->first ) { // 左边有值
-      leftStr = rightStr.substr( 0, iter_down->first - start_index );
-    } else {
-      leftStr = {};
+    //更新关键位置变量：
+    leftDownIndex = min(iter_down->first,start_index);
+    leftUpIndex = min(iter_down->first,start_index+opeStr.size());
+    rightDownIndex =  max(iter_down->first+iter_down->second.size(),start_index);
+    rightUpindex = max(iter_down->first+iter_down->second.size(),start_index+opeStr.size());
+    //截取
+    if(leftDownIndex!=leftUpIndex){
+      data_set.insert({leftDownIndex,opeStr.substr(0,leftUpIndex-leftDownIndex)});
     }
-    if ( start_index + rightStr.size() > iter_down->first + iter_down->second.size()
-         && iter_down->second.size() + iter_down->first > start_index ) { // 右边有值
-      rightStr = rightStr.substr( iter_down->second.size() - start_index + iter_down->first );
-    } else {
-      rightStr = {};
-    }
-    count_insert += leftStr.size();
-    if ( !leftStr.empty() ) {
-      data_set.insert( { start_index, leftStr } );
-    }
-    if ( !rightStr.empty() ) {
-      start_index = iter_down->first + iter_down->second.size();
-    } else {
-      break;
-    }
+    //更新start、opeStr
+    opeStr = rightDownIndex==rightUpindex ? "" : opeStr.substr(rightDownIndex-start_index,rightUpindex-rightDownIndex);
+    start_index = rightDownIndex;
+    count_insert += leftUpIndex-leftDownIndex;
   }
-  // 如果iter_down取值为end，说明到头了，直接把rightStr插入
-  if ( iter_down == data_set.end() ) {
-    // 这里就需要把右边也插入了
-    data_set.insert( { start_index, rightStr } );
-    count_insert += rightStr.size();
-    return count_insert;
-  }
-  // 对于最后的iter_down == iter_up != end的情况，要再来一次切割.ps:这个情况好像能把空的情况合并了
-  if ( iter_down == iter_up && iter_down != data_set.end() && !rightStr.empty() ) {
-    // 先对边界情况进行检验，看看是否有在iter_down的左右边是否有字符串
-    if ( start_index < iter_down->first ) { // 左边有值
-      leftStr = rightStr.substr( 0, iter_down->first - start_index );
-    } else {
-      leftStr = {};
-    }
-    if ( start_index + rightStr.size() > iter_down->first + iter_down->second.size() ) { // 右边有值
-      rightStr = rightStr.substr( iter_down->second.size() - start_index + iter_down->first );
-    } else {
-      rightStr = {};
-    }
-    count_insert += leftStr.size();
-    if ( !leftStr.empty() ) {
-      data_set.insert( { start_index, leftStr } );
-    }
-    if ( !rightStr.empty() ) {
-      // 这里就需要把右边也插入了
-      data_set.insert( { start_index, rightStr } );
-      count_insert += rightStr.size();
-    }
+  if(!opeStr.empty()){
+    data_set.insert({rightDownIndex,opeStr});
+    count_insert += opeStr.size();
   }
   return count_insert;
 }
